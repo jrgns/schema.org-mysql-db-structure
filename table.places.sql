be7b513a5d8c7be8eb1800e62e9bd3ff1d2c294e
Jurgens du Toit <jrgns@jrgns.net>
2011-06-26 07:35:27 +0200

    Added table.places.sql

---
+++ table.places.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `places` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `thing_id` bigint(20) NOT NULL COMMENT 'Link to things table',
+  `parent_id` bigint(20) DEFAULT NULL COMMENT 'Link to places table',
+  `address_id` bigint(20) DEFAULT NULL COMMENT 'Link to postal_addresses table',
+  `geo_id` bigint(20) DEFAULT NULL COMMENT 'Link to geo_coordinates table',
+  `telephone` varchar(255) DEFAULT NULL,
+  `fax_number` varchar(255) DEFAULT NULL,
+  `interaction_count` varchar(1024) DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `added` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+)
+COMMENT='Defined at http://schema.org/Place'
+;
--- table.postal_addresses.sql
+++ table.postal_addresses.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `postal_addresses` (
-  `id` bigint(20) NOT NULL,
-  `thing_id` bigint(20) NOT NULL,
-  `contact_point_id` bigint(20) DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `thing_id` bigint(20) NOT NULL COMMENT 'Link to things table',
+  `contact_point_id` bigint(20) DEFAULT NULL COMMENT 'Link to contact_points table',
   `post_office_box_number` varchar(255) DEFAULT NULL,
   `street_address` varchar(255) DEFAULT NULL,
   `address_locality` varchar(255) DEFAULT NULL,
